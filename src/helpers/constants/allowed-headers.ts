34fefdb9e852fc962268ccc3a8d7f2ec8b4b53df
Lucas M Cardoso <clucasmedeiros@gmail.com>
2020-07-05 12:04:46 -0300

    feat: add allowed-headers

---
+++ src/helpers/constants/allowed-headers.ts
@@ -0,0 +1,10 @@
+export default [
+    'X-Requested-With',
+    'Accept',
+    'Origin',
+    'Referer',
+    'User-Agent',
+    'Content-Type',
+    'Authorization',
+    'X-Mindflash-SessionID',
+];
